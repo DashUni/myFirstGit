d66e967a262ad44b255b40ee8c1fed96b36941ef
Darshan <darshan.naidu@griffithuni.edu.au>
2023-07-28 15:27:07 +1000

    added adding function

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+//Task 1
+//Hello Function
 function hello() {
     return "Hello Week 2"
 }
 
-console.log(hello())
+console.log(hello());
+
+
+
+//Task 4
+//Add Function
+function add(x, y) {
+    return x + y;
+}
+
+console.log("Sum of numbers are: " + add(5, 10));
+
+
